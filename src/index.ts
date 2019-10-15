e5dfdebe6b1a12d6edd0a40c9fb839998b2a2928
zsparal <paral.zsolt@mirango.hu>
2019-10-15 13:20:09 +0200

    Relax reducer type requirements

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ interface ReducerDefinitionMap {
   [key: string]: ReducerDefiniton<any, any>;
 }
 
-type Reducer<S> = (state: S, action: { type: any }) => S;
+type Reducer<S> = (state: S, action: any) => S;
 
 interface ReducerMap {
   [key: string]: Reducer<any>;
